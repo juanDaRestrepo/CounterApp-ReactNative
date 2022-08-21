6f4c907a7075dacc699e6e8e488cbf90bc7e17e1
Daniel Restrepo <jdrestrepo@unitecnica.net>
2022-08-20 21:53:55 -0500

    Working on counter buttons styles

--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,26 +1,63 @@
-import React, { useState } from 'react'
-import { Button, Text, View } from 'react-native'
+import React, {useState} from 'react';
+import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
 
 export const CounterScreen = () => {
-
-   const [counter, setCounter] = useState(10);
+  const [counter, setCounter] = useState(10);
 
   return (
-    <View style={{
-        flex: 1,
-        justifyContent: 'center'
-    }}>
-        <Text style={{ 
-            textAlign: 'center',
-            fontSize: 40,
-
-        }}>
-            Counter : {counter}
-        </Text>
-        <Button 
-            title='click'
-            onPress={() => setCounter( counter + 1 )}
-        />
+    <View style={styles.container}>
+      <Text style={styles.title}>Counter : {counter}</Text>
+      <TouchableOpacity 
+            onPress={() => setCounter(counter + 1)}
+            style={styles.fabLocationBR}
+        >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity 
+            onPress={() => setCounter(counter - 1)}
+            style={styles.fabLocationBL}
+        >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 40,
+    top: -15
+  },
+  fabLocationBR: {
+    position: 'absolute',
+    bottom: 25,
+    right: 25
+  },
+  fabLocationBL: {
+    position: 'absolute',
+    bottom: 25,
+    left: 25
+  },
+  fab : {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center'
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center'
+  }
+});
