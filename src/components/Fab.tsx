c4cb771e108a87437eb7744049845d3e1a6d5aac
Daniel Restrepo <jdrestrepo@unitecnica.net>
2022-08-21 19:44:54 -0500

    Working on Fab component

---
+++ src/components/Fab.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+interface Props {
+  title: string;
+  position?: 'br' | 'bl';
+  onPress: () => void;
+}
+
+export const Fab = ({title, onPress, position = 'br'}: Props) => {
+  return (
+    <View
+      style={[
+        styles.fabLocation,
+        position === 'bl' ? styles.left : styles.right,
+      ]}>
+      <TouchableNativeFeedback 
+        onPress={onPress}
+        background= { TouchableNativeFeedback.Ripple('#28425B', false, 30)}    
+    >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>{title}</Text>
+        </View>
+      </TouchableNativeFeedback>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  fabLocation: {
+    position: 'absolute',
+    bottom: 25,
+  },
+  right: {
+    right: 25,
+  },
+  left: {
+    left: 25,
+  },
+  fab: {
+    backgroundColor: '#5856D6',
+    width: 60,
+    height: 60,
+    borderRadius: 100,
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+
+    elevation: 5,
+  },
+  fabText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: 'bold',
+    alignSelf: 'center',
+  },
+});
--- src/screens/CounterScreen.tsx
+++ src/screens/CounterScreen.tsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react';
 import {Text, TouchableOpacity, View, StyleSheet} from 'react-native';
+import { Fab } from '../components/Fab';
 
 export const CounterScreen = () => {
   const [counter, setCounter] = useState(10);
@@ -7,22 +8,16 @@ export const CounterScreen = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Counter : {counter}</Text>
-      <TouchableOpacity 
-            onPress={() => setCounter(counter + 1)}
-            style={styles.fabLocationBR}
-        >
-        <View style={styles.fab}>
-          <Text style={styles.fabText}>+1</Text>
-        </View>
-      </TouchableOpacity>
-      <TouchableOpacity 
-            onPress={() => setCounter(counter - 1)}
-            style={styles.fabLocationBL}
-        >
-        <View style={styles.fab}>
-          <Text style={styles.fabText}>-1</Text>
-        </View>
-      </TouchableOpacity>
+      <Fab 
+        title='+1' 
+        onPress={() => setCounter(counter +1)}
+        position='br'
+      />
+      <Fab 
+        title='-1' 
+        onPress={() => setCounter(counter -1)}
+        position='bl'
+      />
     </View>
   );
 };
@@ -36,28 +31,5 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 40,
     top: -15
-  },
-  fabLocationBR: {
-    position: 'absolute',
-    bottom: 25,
-    right: 25
-  },
-  fabLocationBL: {
-    position: 'absolute',
-    bottom: 25,
-    left: 25
-  },
-  fab : {
-    backgroundColor: '#5856D6',
-    width: 60,
-    height: 60,
-    borderRadius: 100,
-    justifyContent: 'center'
-  },
-  fabText: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-    alignSelf: 'center'
   }
 });
